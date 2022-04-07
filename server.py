d0bf9a24a48d8365b61275886c1fd99317f92af7
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-04-07 21:50:29 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,53 @@
 import socketserver
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-def some_function():
-    return '1.09'
+version = 0
+versionIncrementation = 1
+strings = ["as", ""]
+os.chdir("c:/Users/yair medina/Desktop/Projects/JS-Idea-Generator")
+
+def GetLatestVersion():
+    global strings
+    global version
+    if(GetAllFiles() != strings or strings == null):
+        strings = GetAllFiles()
+        version = version + versionIncrementation
+        return version
+
+def GetAllFiles():
+    return os.listdir(".")
+
+def GetFile(name):
+    if(name.startswith('.') == False and "." in name):
+        f = open("{}".format(name), "r")
+        return (f.read())
+    else:
+        return ("1")
+    
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if '/update' in self.path:
-
             b = self.path.replace("/update", "")
-            a = some_function()
+            a = GetLatestVersion()
             if(float(a) > float(b)):
-                self.send_response(200) 
+                a = GetAllFiles()
+                self.send_response(200, message = None) 
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
-                self.wfile.write(bytes("you need to update to version {}!".format(a).encode("utf-8")))
-                 
+                self.wfile.write(bytes("{}".format(a).encode("utf-8")))
             else:
-                self.send_response(200) 
+                self.send_response(200, message = None) 
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
                 self.wfile.write(bytes("you dont need to update!".encode("utf-8")))
+        else:
+            self.send_response(200, message = None) 
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("{}".format(GetFile(self.path.replace("%20", " ").replace("/", ""))).encode("utf-8")))
 
 httpd = socketserver.TCPServer(("", 8080), MyHandler)
 httpd.serve_forever()
